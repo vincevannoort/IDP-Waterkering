7fd8d5c6e76604247acd0f75672e20e78d34a145
Vince van Noort <vince.vannoort@student.hu.nl>
2017-01-23 14:16:28 +0100

    Change import statements

--- applicatie/waterkering/routing.py
+++ applicatie/waterkering/routing.py
@@ -1,5 +1,5 @@
 from channels.routing import route
-from waterkering.consumers import ws_add, ws_message, ws_disconnect
+from waterkering.consumers import connect, update, disconnect
 
 channel_routing = [
     route("websocket.connect", connect),
